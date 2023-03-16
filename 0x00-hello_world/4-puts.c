5329d72073281536ab160c862aa985ee3dd8b13e
Albertdeku <albertdeku1498@gmail.com>
2023-03-16 16:24:51 -0700

    Initial commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 /**
- * main - A program that prints aline with puts function
- * return: 0 (Success)
+ * main - A program that prints a line with puts function
+ * Return: 0 (Success)
  */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
-
