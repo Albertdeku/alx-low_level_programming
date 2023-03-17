b635b812a7e058428c8aea3b152c93b15159ea85
Albertdeku <albertdeku1498@gmail.com>
2023-03-17 16:57:42 -0700

    Task 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -5,17 +5,13 @@
  */
 int main(void)
 {
-int i, j;
+int i;
 for (i = 0; i < 10; i++)
 {
-for (j = i; j < 10; j++)
-{
-putchar(i);
-putchar(',');
-if (i != 9 || j != 9)
+putchar(i + '0');
+if (i != 9)
 {
 putchar(',');
-}
 }
 }
 return (0);
