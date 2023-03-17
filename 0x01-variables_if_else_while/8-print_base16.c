c4179ad6cff16f98b25d1f9cad8dc1aa89519bc9
Albertdeku <albertdeku1498@gmail.com>
2023-03-17 15:44:19 -0700

    Task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main -  prints all the numbers of base 16 in lowercase
+ * Return: Always (Success)
+ */
+int main(void)
+{
+int i = 0;
+while (i < 10)
+{
+putchar(i + '0');
+i++;
+}
+i = 0;
+while (i < 6)
+{
+putchar(i + 'a');
+i++;
+}
+putchar('\n');
+return (0);
+}
