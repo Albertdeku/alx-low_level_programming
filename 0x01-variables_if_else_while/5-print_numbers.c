397ccdff108b1221c8e969c25e9dc964ad5b84ee
Albertdeku <albertdeku1498@gmail.com>
2023-03-17 15:26:59 -0700

    Task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - a program that prints all single digit number
+ * Return: Always (Success)
+ */
+int main(void)
+{
+int i = 0;
+while (i < 10)
+{
+printf("%d ", i);
+i++;
+}
+printf("\n");
+return (0);
+}
