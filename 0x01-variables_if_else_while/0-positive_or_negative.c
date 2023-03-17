1b24f6f425abb66b6a323d4d93eb90723ad6061c
Albertdeku <albertdeku1498@gmail.com>
2023-03-17 12:46:18 -0700

    task 1

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -10,15 +10,15 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (n > 0)
 {
-printf("%d is positive\n", n);
+printf("is positive\n");
 }
-else if (n < 0)
+else if (n == 0)
 {
-printf("%d is negative\n", n);
+printf("is zero\n");
 }
 else
 {
-printf("%d is neither positive nor negative\n", n);
+printf("is negative\n");
 }
 return (0);
 }
