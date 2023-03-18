7cd989a1f152d48c5adafe062d33c55040889e92
Albertdeku <albertdeku1498@gmail.com>
2023-03-17 18:31:54 -0700

    y

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -6,19 +6,22 @@
 int main(void)
 {
 int i, j;
-for (i = 0; i <= 9; i++)
+for (i = 0; i < 100; i++)
 {
-for (j = i + 1; j <= 98; j++)
+for (j = i; j < 100; j++)
+{
+if (i != j)
 {
 putchar('0' + (i / 10));
 putchar('0' + (i % 10));
 putchar(' ');
 putchar('0' + (j / 10));
 putchar('0' + (j % 10));
-if (i == 98 && j == 99)
+if (i != 99 || j != 98)
 {
-putchar(' ');
 putchar(',');
+putchar(' ');
+}
 }
 }
 }
