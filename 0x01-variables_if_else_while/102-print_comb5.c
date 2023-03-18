80ff46ef6ffc78ce7864595c95a9b21640747187
Albertdeku <albertdeku1498@gmail.com>
2023-03-17 18:14:34 -0700

    Task 102

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -10,12 +10,13 @@ for (i = 0; i < 100; i++)
 {
 for (j = i; j < 100; j++)
 {
-if (i != j) {
-putchar('0' + i / 10);
-putchar('0' + i % 10);
+if (i != j)
+{
+putchar('0' + (i / 10));
+putchar('0' + (i % 10));
 putchar(' ');
-putchar('0' + j / 10);
-putchar('0' + j % 10);
+putchar('0' + (j / 10));
+putchar('0' + (j % 10));
 if (i != 99 || j != 98)
 {
 putchar(',');
