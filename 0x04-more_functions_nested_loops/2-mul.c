90ede15edb16814681ea19ebaed7edebe3ed4666
Albertdeku <albertdeku1498@gmail.com>
2023-03-23 09:57:06 -0700

    task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ *mul - multiply two numbers
+ *@a: first number
+ *@b: second number
+ * Return: 0 (success)
+ */
+int mul(int a, int b)
+{
+	int result;
+
+	result = a * b;
+	return (result);
+}
