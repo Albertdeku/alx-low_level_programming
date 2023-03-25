3c5d0a615b3426cffbd7d6ae495dd069428e09b3
Albertdeku <albertdeku1498@gmail.com>
2023-03-25 09:35:26 -0700

    Task 100

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <math.h>
+/**
+* main - prints the largest prime factor 
+* Return: Always 0 (succes)
+*/
+int main(void)
+{
+	long x, max, number;
+	double square;
+	number = 612852475143;
+	square = sqrt(number);
+
+	for (x = 1; x <= square; x++)
+	{
+		if (number % x == 0)
+		{
+			max = number / x;
+		}
+	}
+	printf("%ld\n", max);
+	return (0);
+}
