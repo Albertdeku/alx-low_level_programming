16c203933be35f7769729470dbcd417f4c6739a6
Albertdeku <albertdeku1498@gmail.com>
2023-03-27 18:38:10 -0700

    all

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#define PASSWORD_LENGTH 10
+/**
+ *main - start point and end
+ *Return: 0 (Success)
+ */
+int main(void)
+{
+char password[PASSWORD_LENGTH + 1];
+const char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+const size_t charset_size = sizeof(charset) - 1;
+
+srand(time(NULL));
+for (int i = 0; i < PASSWORD_LENGTH; i++)
+{
+	password[i] = charset[rand() % charset_size];
+}
+password[PASSWORD_LENGTH] = '\0';
+
+printf("Generated password: %s\n", password);
+
+return (0);
+}
