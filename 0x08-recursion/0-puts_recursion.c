d4b8d294fad1c1f058d84d9181dbd51b4bd95cf4
Albertdeku <albertdeku1498@gmail.com>
2023-04-05 18:02:46 -0700

    Task 0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
-* _put_recursion - Prints out a string
-* @s: input string
-*/
-
+ * _puts_recursion - Function name
+ * @s: input Argument
+ * Return: Always 0 (Success)
+ */
 void _puts_recursion(char *s)
 {
 	if (*s)
@@ -11,6 +11,7 @@ void _puts_recursion(char *s)
 		_putchar(*s);
 		_puts_recursion(s + 1);
 	}
+
 	else
 		_putchar('\n');
 }
